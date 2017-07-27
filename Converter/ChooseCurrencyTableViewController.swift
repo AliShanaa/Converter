8a79ccaa85dcb75033dcf8079bb302fb301c88c3
ashanaakh <ashanaakh@gmail.com>
2017-07-28 02:18:29 +0300

    logic and interface ready

--- Converter/ChooseCurrencyTableViewController.swift
+++ Converter/ChooseCurrencyTableViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ChooseCurrencyTableViewController.swift
-//  CurrencyConverter
-//
-//  Created by Ali on 4/7/17.
-//  Copyright © 2017 Ali. All rights reserved.
-//
-
 import UIKit
 
 protocol ChooseCurrencyTableViewControllerDelegate {
@@ -13,112 +5,112 @@ protocol ChooseCurrencyTableViewControllerDelegate {
 }
 
 class ChooseCurrencyTableViewController: UITableViewController {
-    
+
     // MARK: Properties
-    
-    var filteredData = [(country: String, iso4217: String, on: Bool)]()
-    
+
     var converter: Converter!
-    
+
+    var model: ChooseCurrencyModel!
+
     var delegate: ChooseCurrencyTableViewControllerDelegate?
-    
+
     @IBOutlet weak var searchBar: UISearchBar!
-    
+
     // MARK: UITableViewController
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         converter = delegate!.converter
-        
+
         searchBar.delegate = self
-        searchBar.tintColor = .white
-        
-        filteredData = converter.get()
+
+        model = ChooseCurrencyModel(currencies: converter.filteredCurrencies)
     }
-    
+
     // MARK: Table view data source
-    
+
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-    
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return filteredData.count
+        return model.filteredCurrencies.count
     }
-    
+
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 60
     }
-    
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChooseCurrencyCell", for: indexPath) as! CurrencyTableViewCell
-        
+
         cell.delegateForCheckBoxActions = self
-        
-        cell.name.text = filteredData[indexPath.row].iso4217
-        cell.icon.image = UIImage(named: filteredData[indexPath.row].country.lowercased())
-        
-        cell.country.text = filteredData[indexPath.row].country.capitalized
-        cell.checkbox.on = filteredData[indexPath.row].on
-        
+
+        let currency = model.filteredCurrencies[indexPath.row]
+
+        cell.name.text = currency.iso4217
+        cell.icon.image = UIImage(named: currency.country.lowercased())
+
+        cell.country.text = currency.country.capitalized
+
+        cell.checkbox.on = currency.on
         cell.checkbox.onAnimationType = .fill
         cell.checkbox.offAnimationType = .fill
-        
+
         return cell
     }
-    
+
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        updateView()
+
+        makeSearchBarOk()
+
         let cell = tableView.cellForRow(at: indexPath) as! CurrencyTableViewCell
-        
+
         cell.checkbox.setOn(!cell.checkbox.on, animated: true)
         cell.checkboxValueDidChange()
-    }
-    
-    func updateView() {
-        searchBar.resignFirstResponder()
-        searchBar.showsCancelButton = false
+
+        updateTableView()
     }
 }
 
 extension ChooseCurrencyTableViewController: CurrencyCellDelegateForCheckBox {
     func add(currency: String) {
         converter.addCurrency(with: currency)
+        model.add(with: currency)
     }
-    
+
     func remove(currency: String) {
         converter.deleteCurrency(with: currency)
+        model.delete(with: currency)
+    }
+
+    func updateTableView() {
+        model.filter(text: searchBar.text ?? "")
+    }
+
+    func makeSearchBarOk() {
+        searchBar.resignFirstResponder()
     }
 }
 
 extension ChooseCurrencyTableViewController: UISearchBarDelegate  {
-    
+
     //MARK: - UISearchBar Methods
-    
-    func filter(text: String) {
-        filteredData = text.isEmpty ? converter.get() : converter.get().filter {
-            let isISO4217 = $0.iso4217.range(of: text.uppercased()) != nil
-            let hasSearchedText = $0.country.range(of: text, options: .caseInsensitive,
-                                                   range: nil, locale: nil) != nil
-            return isISO4217 || hasSearchedText
-        }
-        tableView.reloadData()
-    }
-    
-    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
-        self.searchBar.showsCancelButton = true
-    }
-    
+
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
-        searchBar.showsCancelButton = false
         searchBar.text = ""
         searchBar.resignFirstResponder()
-        
-        filter(text: "")
+
+        model.filter(text: "")
+        tableView.reloadData()
     }
-    
+
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+    }
+
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        filter(text: searchText)
+        model.filter(text: searchText)
+        tableView.reloadData()
     }
 }
-
--- Converter/Converter.swift
+++ Converter/Converter.swift
@@ -1,11 +1,3 @@
-//
-//  converter.swift
-//  CurrencyConverter
-//
-//  Created by Ali on 4/3/17.
-//  Copyright © 2017 Ali. All rights reserved.
-//
-
 import Foundation
 import SwiftyJSON
 import RealmSwift
@@ -14,55 +6,59 @@ class SaveData: Object {
     dynamic var iso4217 = ""
 }
 
+enum Source {
+    case File(String)
+    case Web(String)
+}
+
+enum ConverterError: Error {
+    case NotAnISO4217
+}
+
+struct Currency {
+    var country: String
+    var number: Double
+}
+
 class Converter {
     typealias ISO4217 = String
-    
-    enum Source {
-        case File(String)
-        case Web(String)
-    }
-    
-    enum ConverterError: Error {
-        case NotAnISO4217
-    }
-    
-    struct Currency {
-        var country: String
-        var number: Double
-    }
-    
-    let realm = try! Realm()
-    
-    // currencies user preffered
+
+    // User preffered currencies
     private var dictionary = [ISO4217 : Currency]()
-    
-    // currencies from api
+
+    // From API
     private var currencies = [ISO4217 : Currency]()
-    
-    // list of data changed by searchbar
+
+    // Data changed by searchBar
     private var filteredDictionaryAsArray = [(key: ISO4217, value: Currency)]()
-    
+
     private var koef: Double = 100
-    
-    // currencies from api presented as array
-    private var currenciesAsArray: [(key: ISO4217, value: Currency)] {
-        return currencies.sorted{ $0.key < $1.key }
-    }
-    // number of currencies to show
+
+    // Number of currencies to show
     var numberOfCurrencies: Int {
         return filteredDictionaryAsArray.count
     }
-    
-    // function to convert values
+
+    var filteredCurrencies: [(country: String, iso4217: ISO4217, on: Bool)] {
+        var array = [(country: String, iso4217: ISO4217, on: Bool)]()
+        for currency in currencies {
+
+            let x = dictionary.contains { $0.key == currency.key }
+            array.insert((currency.value.country, currency.key, on: x), at: array.endIndex)
+        }
+        return array.sorted(by: { $0.iso4217 < $1.iso4217 })
+    }
+
+    // Convert values
     func convert(str: ISO4217, value: Double) {
         let oldValue = currencies[str]!.number
         dictionary.forEach {
             dictionary[$0.key]!.number = currencies[$0.key]!.number * value / oldValue
         }
     }
-    
-    // filter values for searchBar
-    func filter(with text: String) {
+
+    // Filter values for searchBar
+    func filterChoosenCurrencies(with text: String) {
         guard !text.isEmpty else {
             filteredDictionaryAsArray = dictionary.sorted{ $0.key < $1.key }
             return
@@ -74,45 +70,34 @@ class Converter {
             return isISO4217 || hasSearchedText
         }
     }
-    
-    // currency information by index for cell
+
+    // Currency information by index for cell
     func get(with index: Int) -> (country: String, iso4217: ISO4217, number: String){
-        
+
         let currency = filteredDictionaryAsArray[index]
-        
+
         var value = currency.value.number
         value = round(value * koef) / koef
-        
+
         let stringValue = String(value)
-        
+
         let val = stringValue.components(separatedBy: ".")
-        
+
         switch val[1] {
         case "0": return (currency.value.country, currency.key, val[0])
         default: return (currency.value.country, currency.key, stringValue)
         }
     }
-    
-    // currency information by iso4217
+
+    // Currency information by ISO4217
     func getRate(with iso4217: ISO4217) throws -> String {
         guard let index = filteredDictionaryAsArray.index(where: { $0.key == iso4217 }) else {
             throw ConverterError.NotAnISO4217
         }
         return get(with: index).number
     }
-    
-    // currencies array as [(country: String, iso4217: ISO4217, on: Bool)]
-    func get() -> [(country: String, iso4217: ISO4217, on: Bool)] {
-        var array = [(country: String, iso4217: ISO4217, on: Bool)]()
-        for currency in currencies {
-            
-            let x = dictionary.contains { $0.key == currency.key }
-            array.insert((currency.value.country, currency.key, on: x), at: array.endIndex)
-        }
-        return array.sorted(by: { $0.iso4217 < $1.iso4217 })
-    }
-    
-    // We dont have some of the api currencies, so need to delete them from currencies
+
+    // We dont have some of the API currencies, so need to delete them from currencies
     func clear() {
         currencies.removeValue(forKey: "CNH")
         currencies.removeValue(forKey: "GGP")
@@ -126,75 +111,86 @@ class Converter {
         currencies.removeValue(forKey: "ZMK")
         currencies.removeValue(forKey: "SHP")
     }
-    
-    private func getCurrencyData(from source: Source) -> JSON? {
-        
+
+    private func getData(from source: Source) -> JSON? {
+
         var data: Data?
-        
+
         if case .File(let path) = source {
-            
+
             guard let text = try? String(contentsOfFile: path) else {
                 return nil
             }
-            
+
             data = text.data(using: .utf8)
-            
+
         } else if case .Web(let adress) = source {
-            
+
             guard let url = URL(string: adress) else {
                 return nil
             }
             data = try? Data(contentsOf: url)
         }
-        
+
         guard let dataForJson = data else {
             return nil
         }
-        
+
         return JSON(data: dataForJson)
     }
-    
-    // delete currency from array by its iso4217
+
+    // Delete currency from array by its iso4217
     func deleteCurrency(with iso4217: ISO4217) {
+
         dictionary.removeValue(forKey: iso4217)
+
         let index = filteredDictionaryAsArray.index(where: { $0.key == iso4217 })!
         filteredDictionaryAsArray.remove(at: index)
-        
-        let indexR = realm.objects(SaveData.self).index(where: {$0.iso4217 == iso4217 })
-        let value = realm.objects(SaveData.self)[indexR!]
-        
-        try! realm.write {
-            
-            realm.delete(value)
+
+        DispatchQueue(label: "background").async {
+
+            // Realm
+            let realm = try! Realm()
+            let indexR = realm.objects(SaveData.self).index(where: {$0.iso4217 == iso4217 })
+            let value = realm.objects(SaveData.self)[indexR!]
+
+            try! realm.write {
+
+                realm.delete(value)
+            }
         }
     }
-    
-    //  currency to array from list of currencies by its iso4217
+
+    // Currency to array from list of currencies by its iso4217
     func addCurrency(with iso4217: ISO4217) {
         dictionary[iso4217] = currencies[iso4217]
-        
+
         if let currency = dictionary.first(where: { $0.key != iso4217 }) {
             convert(str: currency.key, value: currency.value.number)
         }
-        
+
         filteredDictionaryAsArray.append((key: iso4217, value: dictionary[iso4217]!))
         filteredDictionaryAsArray.sort(by: { $0.0.key < $0.1.key })
-        
-        //Realm
-        try! realm.write {
-            realm.add(SaveData(value: [iso4217]))
+
+        DispatchQueue(label: "background").async {
+            //Realm
+            let realm = try! Realm()
+            try! realm.write {
+                realm.add(SaveData(value: [iso4217]))
+            }
         }
     }
-    
-    // called when application begin to read data
+
+    // When app begins reading data
     func loadData() {
-        
+        let realm = try! Realm()
+
         let data = realm.objects(SaveData.self)
         for value in data {
             dictionary[value.iso4217] = currencies[value.iso4217]
         }
-        
-        // if we just download the application
+
+        // If we just download the app
         if dictionary.isEmpty {
             let standartPack = ["USD","EUR","RUB","UAH","GBP","AUD"]
             for iso4217 in standartPack {
@@ -205,45 +201,81 @@ class Converter {
             }
         }
     }
-    
+
     init?(source: String) {
-        
-        // get data from web api
-        
-        guard var jsonFromWeb = getCurrencyData(from: .Web(source)) else {
+
+        // Get data from WEB API
+
+        guard var jsonFromWeb = getData(from: .Web(source)) else {
             return nil
         }
-        
+
         guard var data = jsonFromWeb["rates"].dictionaryObject as? [String : Double] else {
             return nil
         }
-        
+
         data[jsonFromWeb["base"].string!] = 1
-        
+
         for currency in data {
             currencies[currency.key] = Currency(country: "", number: currency.value)
         }
-        
+
         clear()
-        
+
         guard let path = Bundle.main.path(forResource: "countries", ofType: "json") else {
             return nil
         }
-        
-        guard let jsonFromFile = getCurrencyData(from: .File(path)) else {
+
+        guard let jsonFromFile = getData(from: .File(path)) else {
             return nil
         }
-        
+
         guard let countries = jsonFromFile["rates"].dictionaryObject as? [String : String] else {
             return nil
         }
-        
+
         currencies.forEach {
             currencies[$0.key]!.country = countries[$0.key] ?? "No Country!"
         }
-        
+
         loadData()
-        
-        filteredDictionaryAsArray = dictionary.sorted{ $0.key < $1.key }
+
+        filteredDictionaryAsArray = dictionary.sorted { $0.key < $1.key }
     }
+
 }
+
+class ChooseCurrencyModel {
+    typealias ISO4217 = String
+
+    private var currencies = [(country: String, iso4217: ISO4217, on: Bool)]()
+    var filteredCurrencies = [(country: String, iso4217: ISO4217, on: Bool)]()
+
+    func filter(text: String) {
+        filteredCurrencies = text.isEmpty ? currencies : currencies.filter {
+            let isISO4217 = $0.iso4217.range(of: text.uppercased()) != nil
+            let hasSearchedText = $0.country.range(of: text, options: .caseInsensitive,
+                                                   range: nil, locale: nil) != nil
+            return isISO4217 || hasSearchedText
+        }
+    }
+
+    func add(with iso4217: ISO4217) {
+        let index = currencies.index(where: { $0.iso4217 == iso4217})!
+        currencies[index].on = true
+    }
+
+    func delete(with iso4217: ISO4217) {
+        let index = currencies.index(where: { $0.iso4217 == iso4217})!
+        currencies[index].on = false
+    }
+
+    init(currencies: [(country: String, iso4217: ISO4217, on: Bool)]) {
+        self.currencies = currencies
+        filteredCurrencies = currencies
+    }
+
+}
+
+
+
